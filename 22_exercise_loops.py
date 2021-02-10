26316ec23f83d3bef2e69036c25ae549b56ee214
charlespeters <charles29020@me.com>
2021-02-09 21:06:23 -0500

    adad

---
+++ 22_exercise_loops.py
@@ -0,0 +1,12 @@
+# 22_exercise_loops.py
+
+'''
+Exercise: Loopa
+1) Create a program that ask the user for 8 names of people and
+store them in a list. When all the names have been given, pick a
+random one and print it.
+2) Create a guess game with the names of the colors. At each round
+pick a random color from a list and let the user try to guess it. When he
+does it, ask if he wants to play again. Keep playing until the user types
+"no".
+'''
